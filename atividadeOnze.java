c68bf9fd93027243e48f4eb54c6827bc419a039a
SorenAxelsen <163474970+SorenAxelsen@users.noreply.github.com>
2024-04-12 02:29:06 -0300

    Update atividadeOnze.java

--- atividadeOnze.java
+++ atividadeOnze.java
@@ -8,7 +8,7 @@ public class atividade11 {
         int contadorMais90Kg = 0;
         int somaIdades = 0;
 
-        // Loop para receber idade e peso de sete pessoas
+        
         for (int i = 0; i < totalPessoas; i++) {
             System.out.print("Digite a idade da pessoa: ");
             int idade = scanner.nextInt();
@@ -16,19 +16,19 @@ public class atividade11 {
             System.out.print("Digite o peso (em kg) da pessoa: ");
             float peso = scanner.nextFloat();
 
-            // Verifica se o peso é superior a 90 quilos
+            
             if (peso > 90) {
                 contadorMais90Kg++;
             }
 
-            // Soma das idades
+            
             somaIdades += idade;
         }
 
-        // Cálculo da média das idades
+        
         float mediaIdades = (float) somaIdades / totalPessoas;
 
-        // Exibição dos resultados
+       
         System.out.println("Quantidade de pessoas com mais de 90 quilos: " + contadorMais90Kg);
         System.out.println("Média das idades das sete pessoas: " + mediaIdades);
 
